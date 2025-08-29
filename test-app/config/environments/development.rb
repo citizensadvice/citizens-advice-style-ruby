09b082a59c4615493b78972bc4bee7249bd7563e
David Rapson <david.rapson@citizensadvice.org.uk>
2025-08-29 11:11:49 +0100

    Fix lint error

--- test-app/config/environments/development.rb
+++ test-app/config/environments/development.rb
@@ -4,7 +4,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
